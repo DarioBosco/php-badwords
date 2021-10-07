ead6aa3aae86b1eef16f0583d7e82ad3cbf2fbcd
DarioBosco <dario_bosco@protonmail.com>
2021-10-07 15:50:06 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 	$string = 'Primo esercizio con PHP';
-	$bannedWord
-
+	$bannedWord = $_GET['bannedWord'];
+	$censoredString = str_ireplace($bannedWord, '***', $string);
 ?>
 
 
 <h1>Frase: <?php echo $string ?>  </h1>
 <h2>Lunghezza della frase: <?php  echo strlen($string) ?> </h2>
+<h2>Parola non consentita: <?php echo $bannedWord ?> </h2>
+<p> Frase Censurata: <?php echo $censoredString ?> </p>
+<p>Lunghezza della frase censurata: <?php  echo strlen($censoredString) ?> </p>
